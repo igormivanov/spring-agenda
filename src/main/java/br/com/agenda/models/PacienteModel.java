6ed921a575b186e3b005399f313e9695fc708642
igormivanov <igormivanovx@gmail.com>
2023-02-04 22:36:03 -0300

    Criando as entidades da aplicação

---
+++ src/main/java/br/com/agenda/models/PacienteModel.java
@@ -0,0 +1,25 @@
+package br.com.agenda.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "paciente")
+public class PacienteModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String cpf;
+	
+}
